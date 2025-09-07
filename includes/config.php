701968647d8e64748586d54fc2065f88a14382b0
Wido <wicax2020@gmail.com>
2025-09-07 15:49:47 +0700

    Mengubah nama file dari nama_file_lama.php menjadi nama_file_baru.php

--- includes/config.php
+++ includes/config.php
@@ -6,10 +6,10 @@ try {
     // Cek apakah aplikasi berjalan di Railway (menggunakan variabel lingkungan)
     if (getenv('RAILWAY_ENVIRONMENT')) {
         // Konfigurasi untuk Railway
-        $host = getenv('MYSQL_HOST');
+        $host = getenv('MYSQLHOST');
         $dbname = getenv('MYSQL_DATABASE');
-        $user = getenv('MYSQL_USER');
-        $pass = getenv('MYSQL_PASSWORD');
+        $user = getenv('MYSQLUSER');
+        $pass = getenv('MYSQLPASSWORD');
 
         // Buat koneksi PDO untuk Railway
         $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
